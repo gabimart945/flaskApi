7925d6a8122a7d39d9fea2b3b3ddd62d97727c9e
Gabi Martinez Pieper <gabrielmartinez945@comunidadunir.net>
2024-11-10 16:51:48 +0100

    working

--- config.py
+++ config.py
@@ -2,8 +2,5 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "postgresql://postgres:P0sgre$s1stemas@database.ctm6m1rqejhv.us-east-1.rds.amazonaws.com:5432/postgres"
-    )
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:P0sgre$s1stemas@database.ctm6m1rqejhv.us-east-1.rds.amazonaws.com:5432/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
